ab6e62307b6871609a63348903f6121972fda64e
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-04-26 10:54:14 +0900

    feat : make project

---
+++ app.js
@@ -0,0 +1,19 @@
+// express 모듈을 불러옵니다.
+const express = require('express');
+
+// express 애플리케이션을 생성합니다.
+const app = express();
+// 정적 파일 제공을 위한 미들웨어 설정
+app.use(express.static('public'));
+// 웹 서버가 사용할 포트 번호를 정의합니다.
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
