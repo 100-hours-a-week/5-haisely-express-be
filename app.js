ba17a71c6299096927a2c944769a8889368af775
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-04-27 23:18:41 +0900

    feat : api 추가(post)

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // express 모듈을 불러옵니다.
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // express 애플리케이션을 생성합니다.
 const app = express();
 // 정적 파일 제공을 위한 미들웨어 설정
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // 웹 서버가 사용할 포트 번호를 정의합니다.
 const port = 3001;
 
--- routes/boards.js
+++ routes/boards.js
@@ -3,14 +3,34 @@ const fs = require('fs');
 
 const router = express.Router();
 
-// JSON 파일 경로
-const dataFilePath = 'public/data/boards.json';
+const boardDataPath = 'public/data/boards.json';
+const commentDataPath = 'public/data/comments.json';
+const keyDataPath = 'public/data/keys.json';
 
-// JSON 파일 로드
-let boardData = JSON.parse(fs.readFileSync(dataFilePath));
+const loadData = (dataFilePath) => {
+    try {
+        const data = fs.readFileSync(dataFilePath, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error('Error reading file:', err);
+        return null;
+    }
+}
+
+const saveData = (data, filePath) => {
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+        console.log('Data saved successfully');
+    } catch (err) {
+        console.error('Error saving file:', err);
+    }
+}
+
+
 
 // 게시글 목록 조회
 router.get('/', (req, res) => {
+    const boardData = loadData(boardDataPath);
     const { offset, limit } = req.query;
     jsonData = {
         "status" : 200, "message" : null, "data" : boardData
@@ -20,6 +40,7 @@ router.get('/', (req, res) => {
 
 // 게시글 상세 조회
 router.get('/:id', (req, res) => {
+    const boardData = loadData(boardDataPath);
     const boardId = req.params.id;
     const board = boardData["boards"].find(board => board.post_id === parseInt(boardId));
     jsonData = {
@@ -27,6 +48,86 @@ router.get('/:id', (req, res) => {
     }
     res.json(jsonData);
 });
+
+// 게시글 추가 "/"
+router.post("/", (req, res) =>{
+    let boardData = loadData(boardDataPath);
+    let keyData = loadData(keyDataPath);
+
+    // user 정보 처리 추가 필요
+
+    postId = keyData.board_id + 1;
+    keyData.board_id += 1;
+    saveData(keyData, keyDataPath);
+
+    const now = new Date();
+    const localTimeString = now.toLocaleString();
+
+    const requestData = req.body;
+    const newData = {
+        "post_id": postId,
+        "post_title": requestData.postTitle,
+        "post_content": requestData.postContent,
+        "file_id": null,
+        "user_id": 1,  // 수정
+        "nickname": "테스트",  // 수정
+        "created_at": localTimeString,
+        "updated_at": localTimeString,
+        "deleted_at": null,
+        "like": "0",
+        "comment_count": "0",
+        "hits": "1",
+        "file_path": requestData.attachFilePath,
+        "profile_image_path": "/public/image/profile/test.jpg"  // 수정
+    };
+
+    boardData.boards.push(newData);
+    saveData(boardData, boardDataPath);
+
+    jsonData = {
+        "status" : 201, "message" : "write_post_success", "data" : {"post_id" : newData.post_id}
+    }
+    res.json(jsonData);
+});
+
+
+// 게시글 수정 "/:id"
+router.patch("/:id", (req, res) =>{
+
+});
+
+
+// 게시글 삭제 "/:id"
+router.delete("/:id", (req, res) =>{
+
+});
+
+
+// 댓글 목록 조회 "/:id/comments"
+router.get("/:id/comments", (req, res) =>{
+    const boardData = loadData(boardDataPath);
+    const commentData = loadData(commentDataPath);
+
+
+});
+
+// 댓글 추가 "/:id/comments"
+router.post("/:id/comments", (req, res) =>{
+
+});
+
+
+// 댓글 수정 "/{post_id}/comments/{comment_id}"
+router.patch("/:postId/comments/:commentId", (req, res) =>{
+
+});
+
+
+// 댓글 삭제 "/{post_id}/comments/{comment_id}"
+router.delete("/:postId/comments/:commentId", (req, res) =>{
+
+});
+
 
 
 module.exports = router;
