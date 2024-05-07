b6f1503ea740e05d16801f6f32213d3f6e48fade
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-05-07 14:31:12 +0900

    feat : add status in boards post

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -1,6 +1,7 @@
 /*CHECKLIST
 [ ] 인증 인가 구현하기
 [ ] 상태코드 401, 403 추가하기
+[ ] 모든 응답 에러에 500 적용하기 -> 어케하징
 */
 
 const {loadData, saveData, makeRes} = require ('./controllerUtils.js');
@@ -20,45 +21,13 @@ const findCommentsByPostId = (id) => {
     return commentData["comments"].filter(item => item.post_id === parseInt(id));
 }
 
-/* Controller */
-const getBoards = (req, res) => {
-    const boardData = loadData(boardDataPath);
-    res.status(200).json(makeRes(200, null, boardData));
-}
-
-const getBoardDetail = (req, res) => {
-    const boardId = req.params.id;
-    const board = findBoardById(boardId);
-    if (board === undefined) {res.status(404).json(makeRes(404, "cannot_found_post", null)); return;}
-    const comments = findCommentsByPostId(boardId);
-    res.status(200).json(makeRes(200, null, {"board" : board, "comments":comments}));
-}
-
-const postBoard = (req, res) =>{
-    const requestData = req.body;
-    if(!requestData.postTitle || !requestData.postContent){
-        console.log("데이터 미포함 요청");
-        jsonData = {
-            "status" : 400, "message" : "invalid", "data" :null
-        }
-        res.json(jsonData);
-        return;
-    }
-
-    let boardData = loadData(boardDataPath);
-    let keyData = loadData(keyDataPath);
-
-    // user 정보 처리 추가 필요
-
-    postId = keyData.board_id + 1;
-
+const makeNewBoard = (user, postTitle, postContent, attachFilePath) => {
     const now = new Date();
     const localTimeString = now.toLocaleString();
-
-    const newData = {
-        "post_id": postId,
-        "post_title": requestData.postTitle,
-        "post_content": requestData.postContent,
+    // need to use user data
+    return {
+        "post_title": postTitle,
+        "post_content": postContent,
         "file_id": null,
         "user_id": 1,  // 수정
         "nickname": "테스트",  // 수정
@@ -68,20 +37,45 @@ const postBoard = (req, res) =>{
         "like": "0",
         "comment_count": "0",
         "hits": "1",
-        "file_path": requestData.attachFilePath  || null,
+        "file_path": attachFilePath  || null,
         "profile_image_path": "/images/default.png"  // 수정
     };
+}
 
-    boardData.boards.push(newData);
+const saveNewBoard = (newBoard) => {
+    let boardData = loadData(boardDataPath);
+    let keyData = loadData(keyDataPath);
+    const postId = keyData.board_id + 1;
+    newBoard.post_id = postId;  // set post_id
+    boardData.boards.push(newBoard);  // push new board
     saveData(boardData, boardDataPath);
-
     keyData.board_id += 1;
     saveData(keyData, keyDataPath);
+    return postId;
+}
 
-    jsonData = {
-        "status" : 201, "message" : "write_post_success", "data" : {"post_id" : newData.post_id}
-    }
-    res.json(jsonData);
+/* Controller */
+const getBoards = (req, res) => {
+    const boardData = loadData(boardDataPath);
+    res.status(200).json(makeRes(200, null, boardData));
+}
+
+const getBoardDetail = (req, res) => {
+    const boardId = req.params.id;
+    const board = findBoardById(boardId);
+    if (!board) {res.status(404).json(makeRes(404, "cannot_found_post", null)); return;}  // board not found
+    const comments = findCommentsByPostId(boardId);
+    res.status(200).json(makeRes(200, null, {"board" : board, "comments":comments}));
+}
+
+const postBoard = (req, res) =>{
+    const requestData = req.body;
+    if(!requestData.postTitle){res.status(400).json(makeRes(400, "invalid_post_title", null)); return;} // invalid title
+    if(!requestData.postContent){res.status(400).json(makeRes(400, "invalid_post_content", null)); return;} // invalid content
+    // need to send user data
+    const newPost = makeNewBoard(null, requestData.postTitle, requestData.postContent, requestData.attachFilePath);
+    const postId = saveNewBoard(newPost);
+    res.status(201).json(makeRes(201, "write_post_success", {"post_id" : postId}));
 }
 
 const patchBoard = (req, res) =>{
